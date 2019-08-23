e08b639589db9060b20ba73788553a7dc0e0f574
Gustavo Souza <gustavo.hmbsouza@gmail.com>
2019-08-23 11:47:29 -0300

    Classes

--- curso-es6/main.js
+++ curso-es6/main.js
@@ -1,19 +1,55 @@
-alert('Olá Mundo')
-
-class teste
+class Lista
 {
-    metodoDoTeste()
+    constructor()
     {
-
+        this.data = [];
     }
 
-    segundoMetodo()
+    add(data)
     {
+        this.data.push(data);
+        console.log(this.data);
+    }
+}
+class TodoList extends Lista
+{
+    constructor()
+    {
+        super();
 
+        this.usuario = "Gustavo Souza";
     }
 
-    terceiroMetodo()
+    mostraUsuario()
     {
-        
+        console.log(this.usuario);
     }
+}
+
+var MinhaLista = new TodoList();
+
+document.getElementById('novotodo').onclick =
+function()
+{
+    MinhaLista.add("Um novo");
+}
+
+document.getElementById('Usuario').onclick =
+function()
+{
+    MinhaLista.mostraUsuario();
+}
+
+class Matematica
+{
+    static soma(a, b)
+    {
+        return a + b;
+    }
+}
+
+document.getElementById('soma').onclick =
+function()
+{
+    console.log(Matematica.soma(2,3));
 }
