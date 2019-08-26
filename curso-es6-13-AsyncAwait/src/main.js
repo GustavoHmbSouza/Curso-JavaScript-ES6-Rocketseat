b844e1b37ac170e56200a75f392b2396359d27b4
Gustavo Souza <gustavo.hmbsouza@gmail.com>
2019-08-26 13:30:09 -0300

    AsyncAwait

---
+++ curso-es6-13-AsyncAwait/src/main.js
@@ -0,0 +1,21 @@
+const minhaPromisse = () => new Promise((resolve, reject) => {
+    setTimeout(() => { resolve('OK')}, 2000);
+});
+
+async function executaPromisse()
+{
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+}
+
+executaPromisse();
+
+const executaPromisse2 = async () =>
+{
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+    console.log(await minhaPromisse());
+}
+
+executaPromisse2();
