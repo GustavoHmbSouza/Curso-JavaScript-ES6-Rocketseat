d5478b4fd940009a33c000f32c1cf56f6fc2fdb7
Gustavo Souza <gustavo.hmbsouza@gmail.com>
2019-08-26 10:41:40 -0300

    Import / Export

---
+++ curso-es6-11-ImportExport/main.js
@@ -0,0 +1,8 @@
+//import { soma as somaFunction, sub } from './funcoes';
+import * as funcoes from './funcoes'; 
+import multiplicacaoFunction from './multiplicacao';
+
+console.log(funcoes.soma(9, 2));
+console.log(funcoes.sub(9, 2));
+
+console.log(multiplicacaoFunction(9, 2));
