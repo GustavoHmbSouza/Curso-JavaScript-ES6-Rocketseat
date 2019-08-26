58bf91e6bf89fb69ba3c3ef6ff5acb208cf66725
Gustavo Souza <gustavo.hmbsouza@gmail.com>
2019-08-26 17:56:21 -0300

    Loading e Error

--- curso-es6-19-LoadingError/src/main.js
+++ curso-es6-19-LoadingError/src/main.js
@@ -1,3 +1,5 @@
+import api from './api';
+
 class App
 {
     constructor()
@@ -5,6 +7,7 @@ class App
         this.repositories = [];
 
         this.formEl = document.getElementById('repo-form');
+        this.inputEl = document.querySelector('input[name=repository]')
         this.listEl = document.getElementById('repo-list');
 
         this.registerHandlers();
@@ -15,19 +18,56 @@ class App
         this.formEl.onsubmit = event => this.addRepository(event);
     }
 
-    addRepository(event)
+    setLoading(loading = true)
+    {
+        if (loading == true)
+        {
+            let loadingEl = document.createElement('span');
+            loadingEl.append(document.createTextNode('Carregando'));
+            loadingEl.setAttribute('id', 'loading');
+        
+            this.formEl.appendChild(loadingEl);
+        }
+        else
+        {
+            document.getElementById('loading').remove();
+        }
+    }
+
+    async addRepository(event)
     {
         event.preventDefault();
 
-        this.repositories.push(
-        {
-            name: 'rocketseat.com.br',
-            description: 'Tire a sua ideia do papel e dê vida a sua startup',
-            avatar_url:'https://avatars0.githubusercontent.com/u/28929274v=4',
-            html_url: 'http://github.com/Rocketseat/unform'
-        });
+        const username = this.inputEl.value;
 
-        this.render();
+        if (username.length === 0)
+            return;
+
+        this.setLoading();
+
+        try
+        {
+            const response = await api.get(`https://api.github.com/repos/Rocketseat/${username}`)
+
+            const { name, description, html_url, owner: { avatar_url } } = response.data;
+
+            this.repositories.push(
+            {
+                name,
+                description,
+                avatar_url,
+                html_url,
+            });
+
+            this.inputEl.value = '';
+            
+            this.render();
+        } catch (err)
+        {
+            alert('O repositório não existe!!');
+        }
+
+        this.setLoading(false);
     }
 
     render()
